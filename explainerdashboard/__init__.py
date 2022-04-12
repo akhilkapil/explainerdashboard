1ada3560566c904fe002cd13ba81c8eb616873a7
Akhil Kumar Kapil <70738578+akhilkapil@users.noreply.github.com>
2022-04-12 14:52:33 +0530

    Update __init__.py

--- explainerdashboard/__init__.py
+++ explainerdashboard/__init__.py
@@ -2,4 +2,4 @@
 ___version__ = "0.3.8.2"
 
 from .explainers import ClassifierExplainer, RegressionExplainer
-from .dashboards import ExplainerDashboard, ExplainerHub, InlineExplainer
+# from .dashboards import ExplainerDashboard, ExplainerHub, InlineExplainer
